9d1c2dc01363ec28227b726c9fb60735f4641e10
rickrobs7 <rickrobs7@gmail.com>
2022-01-06 16:01:14 -0700

    Completed lessons 1-15

---
+++ starter/script.js
@@ -0,0 +1,148 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+const PI = 3.1315;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+
+/*
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
+*/
+
+/*
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; //x = x + 10 = 25
+x *= 4; //x = x * 4 = 100
+x++; //x = x + 1
+x--;
+x--;
+
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // >, <, >= <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+*/
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; //x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
+
+console.log("------------Assignment 1------------");
+
+const country = "The USA";
+const continent = "North America";
+let population = "329.5";
+
+console.log(country, continent, population);
+
+console.log("------------Assignment 2------------");
+
+const isIsland = false;
+let language;
+
+console.log(isIsland, population, country, language);
+
+console.log("------------Assignment 3------------");
+
+language = "English";
+console.log(language);
+
+console.log("------------Assignment 4------------");
+
+population /= 2;
+console.log(population);
+
+population *= 2;
+population++;
+console.log(population);
+population--;
+
+console.log(population > 6);
+
+console.log(population < 33);
+
+const description = `${country} is in ${continent} and its ${population} million people speak ${language}`;
+console.log(description);
